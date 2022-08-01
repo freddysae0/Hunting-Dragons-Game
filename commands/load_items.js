0514ac131cba61bcdc2e809d2cc153a102f2b7b8
freddysae0 <freddysae00@gmail.com>
2022-08-01 10:05:06 -0400

    more items

--- commands/load_items.js
+++ commands/load_items.js
@@ -4,48 +4,92 @@ module.exports = async (ctx) => {
   await Items.sync({ force: true });
   items = [
     {
+      id: 1,
       name: "Old man letter",
       description: `This object contains important information and cannot be deleted`,
       can_be_crafted: false,
     },
     {
+      id: 2,
       name: "Ancient papyrus",
       description: `This object contains important information and cannot be deleted`,
       can_be_crafted: false,
     },
     {
+      id: 3,
       name: "Alpha Wolf Spirit",
       description: `This object contains important information and cannot be deleted`,
       can_be_crafted: false,
     },
     {
+      id: 4,
       name: "Kingdom scriptures",
       description: `This object contains important information and cannot be deleted`,
       can_be_crafted: false,
     },
     {
-      name: "Mesita de grafeteo",
-      description: `Una mesita de grafeteo!!`,
-      can_be_crafted: true,
-      crafted_with_string: "/4/.1.",
+      id: 5,
+      name: "Stick",
+      description: `A very commun object`,
+      can_be_crafted: false,
     },
     {
-      name: "Espada Nordica",
-      description: `Espada con barba q esperabas duh`,
-      can_be_crafted: true,
-      crafted_with_string: "/3/.1.",
+      id: 6,
+      name: "Vegetal Fiber",
+      description: `A very commun object`,
+      can_be_crafted: false,
     },
     {
-      name: "Espada Hawayana",
-      description: `esta espada tiene pi;a`,
-      can_be_crafted: true,
-      crafted_with_string: "/3/.1./1/.2.",
+      id: 7,
+      name: "Sand",
+      description: `A comun object`,
+      can_be_crafted: false,
     },
     {
-      name: "Alain",
-      description: `wapo`,
+      id: 8,
+      name: "Dirt",
+      description: `A comun object`,
+      can_be_crafted: false,
+    },
+    {
+      id: 9,
+      name: "Fairy dust",
+      description: `A comun object`,
+      can_be_crafted: false,
+    },
+    {
+      id: 10,
+      name: "Stone",
+      description: `A very comun object`,
+      can_be_crafted: false,
+    },
+    {
+      id: 11,
+      name: "Thread",
+      description: `Perfect to cover your girlfriend in cold weather. Wait... What girlfriend?`,
+      can_be_crafted: false,
+    },
+    {
+      id: 12,
+      name: "Rope",
+      description: `Tie that up before it slips away`,
       can_be_crafted: true,
-      crafted_with_string: "/3/.1.",
+      crafted_with_string: "/6/.2.",
+    },
+
+    {
+      id: 13,
+      name: "Wood Sword",
+      description: `This hurts:  +3 atk`,
+      can_be_crafted: true,
+      crafted_with_string: "/5/.3.",
+    },
+    {
+      id: 14,
+      name: "Wood Shield",
+      description: `This will protect you:  +4 def`,
+      can_be_crafted: true,
+      crafted_with_string: "/5/.6.",
     },
   ];
 
@@ -57,4 +101,5 @@ module.exports = async (ctx) => {
       crafted_with_string: items[i].crafted_with_string,
     });
   }
+  ctx.reply("Items loaded ");
 };
