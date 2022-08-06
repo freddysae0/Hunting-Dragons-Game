276ae136cff4866960a48c6c91cad3b9684486fc
freddysae0 <freddysae00@gmail.com>
2022-08-06 12:33:47 -0400

    added inline buttons

--- events/explore.js
+++ events/explore.js
@@ -1,8 +1,14 @@
 const Players = require("../models/players");
 const others = require("../others/others");
 const Items = require("../models/items");
+const Markup = require("telegraf/markup");
 /* globals bot */
 
+const ExploreAndMisions = [
+  [Markup.callbackButton("Explore", "Explore")],
+  [Markup.callbackButton("Misiones Disponibles!", "Misiones Disponibles!")],
+];
+const JustExplore = [[Markup.callbackButton("Explore", "Explore")]];
 /**
  * Can be regular expression or String
  * @type {RegExp}
@@ -24,8 +30,19 @@ module.exports.do = async (ctx) => {
   });
 
   if (players.length > 0) {
-    ctx.reply(
-      "Estas a punto de salir a explorar el reino en busca de nuevas aventuras Touch here to continue"
-    );
+    //Este arreglo missions debe contener id de misiones
+    missions = [];
+    if (missions.length == 0) {
+      ctx.reply(
+        "Estas a punto de salir a explorar el reino en busca de nuevas aventuras Touch here to continue:",
+        Markup.inlineKeyboard(JustExplore).extra()
+      );
+    }
+    if (missions.length > 0) {
+      ctx.reply(
+        "Estas a punto de salir a explorar el reino en busca de nuevas aventuras Touch here to continue:",
+        Markup.inlineKeyboard(ExploreAndMisions).extra()
+      );
+    }
   }
 };
