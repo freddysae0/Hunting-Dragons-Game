ce372cf7f0e0d2bc86e3403c90da40c715177b8c
freddysae0 <freddysae00@gmail.com>
2022-08-08 01:02:04 -0400

    fast commit

---
+++ commands/items/i60.js
@@ -0,0 +1,4 @@
+const { use } = require("../../others/items/i60");
+module.exports = (ctx) => {
+  use(ctx);
+};
---
+++ commands/items/i61.js
@@ -0,0 +1,4 @@
+const { use } = require("../../others/items/i61");
+module.exports = (ctx) => {
+  use(ctx);
+};
