7edd8e910c735b77e7e8c7c00faa9369c2b8a3f0
freddysae0 <freddysae00@gmail.com>
2022-08-08 12:13:21 -0400

    bug fixed

--- commands/inv.js
+++ commands/inv.js
@@ -1,5 +1,4 @@
 const Players = require("../models/players");
-const Items = require("../models/items");
 const { Op } = require("sequelize");
 const others = require("../others/others");
 
@@ -15,28 +14,34 @@ module.exports = async (ctx) => {
   inv_string = players[0].dataValues.inv_string;
 
   itemsAndQuantity = others.decifrarInvString(inv_string);
-  var items = await Items.findAll({
-    where: {
-      id: { [Op.or]: itemsAndQuantity.items },
-    },
-  });
+  itemsEnElInv = itemsAndQuantity.items;
+  var tengoElItem = [];
+
+  for (let i = 0; i < itemsEnElInv.length; i++) {
+    tengoElItem[itemsEnElInv[i]] = itemsAndQuantity.quantity[i];
+  }
+  console.log(itemsEnElInv);
+  console.log(tengoElItem);
+
+  console.log(tengoElItem[itemsEnElInv[0]]);
+  console.log(tengoElItem[itemsEnElInv[1]]);
+  console.log(tengoElItem[itemsEnElInv[2]]);
+
+  Items = others.getItemsArray();
 
   var s = `<b>Your inventory:</b>
     `;
 
   if (itemsAndQuantity.items.length == 0) s = "Your inventory is empty";
-  for (let i = 0; i < itemsAndQuantity.items.length; i++) {
-    for (let j = 0; j < items.length; j++) {
-      if (itemsAndQuantity.items[i] == items[j].dataValues.id) {
-        s += items[j].dataValues.name.toString();
-        s += ": ";
-        s += itemsAndQuantity.quantity[i].toString();
-        s += ` <i>--Use:</i> /i`;
-        s += items[j].dataValues.id.toString();
-        s += `
+  for (let i = 0; i < itemsEnElInv.length; i++) {
+    thisItem = others.getItembyId(itemsEnElInv[i]);
+    s += thisItem.name;
+    s += ": ";
+    s += tengoElItem[itemsEnElInv[i]].toString();
+    s += ` <i>--Use:</i> /i`;
+    s += thisItem.id.toString();
+    s += `
     `;
-      }
-    }
   }
   s += ``;
   ctx.replyWithHTML(s);
